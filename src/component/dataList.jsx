a99df1d7ea53011051f6555f5636d4531cefada1
huyongbo <myoldmoons@163.com>
2019-09-30 16:46:18 +0800

    react-admin大框架创建

---
+++ src/component/dataList.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default class List extends React.Component{
+    render(){
+        return(
+            <div>list</div>
+        )
+    }
+}
---
+++ src/component/home.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+// 引入 ECharts 主模块
+import echarts from 'echarts/lib/echarts';
+// 引入柱状图
+import  'echarts/lib/chart/bar';
+// 引入提示框和标题组件
+import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/title';
+export default class home extends React.Component{
+    componentDidMount() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('main'));
+
+        var option = {
+            title: { text: 'ECharts' },
+            color: ['#3398DB'],
+            tooltip : {
+                trigger: 'axis',
+                axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                    type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                }
+            },
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            xAxis : [
+                {
+                    type : 'category',
+                    data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                    axisTick: {
+                        alignWithLabel: true
+                    }
+                }
+            ],
+            yAxis : [
+                {
+                    type : 'value'
+                }
+            ],
+            series : [
+                {
+                    name:'直接访问',
+                    type:'bar',
+                    barWidth: '60%',
+                    data:[10, 52, 200, 334, 390, 330, 220]
+                }
+            ]
+        };
+
+        // 绘制图表
+        myChart.setOption(option);
+    }
+    render() {
+        return (
+            <div id="main" style={{ width: 500, height: 400 }}></div>
+        );
+    }
+}
